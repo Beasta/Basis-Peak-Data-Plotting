56f99b8d69785e05c75ab1aa3579e4e0dc56d3dc
highfalootin <james.harlan@gmail.com>
2015-05-10 15:32:43 -0700

    cleaned up dataImport.js and reordered functions

--- dataImport.js
+++ dataImport.js
@@ -1,8 +1,12 @@
 (function(){
 
 	var biometrics = {};
-	biometrics.data = [];
+	document.biometrics = biometrics;
+	document.biometrics.data = [];
+	biometrics.average = {};
+
 	function importBiometrics(){
+
 		$.ajax({								/*jQuery ajax request*/
 			url:'Data-Files/bodymetrics_bigfile.txt',
 			beforeSend:function(){console.log('Loading Data File...');},
@@ -10,9 +14,30 @@
 			error:function(){console.log('Error importing data');},
 			success:function(){				/*success callback*/
 				console.log('Success!');
-				writeToDiv();
+				onSuccess();
 				}
 		});
+
+		function onSuccess(){
+			for (var j = 2; j < biometrics.header.length; j++) {
+				biometrics.average[biometrics.header[j]] = average(biometrics.header[j]);
+			}
+			for( var key in biometrics.average){
+				writeToDiv(key);
+				writeToDiv(biometrics.average[key]);
+			}
+		}
+
+		function average(key){					/*helper function to compute average values*/
+			var count = 0;
+			var sum = biometrics.data.reduce(function(prev, curr){
+				if(curr[key]){
+					count++;
+					return prev + curr[key];
+				}else{return prev;}
+			},0);
+			return sum/count;
+		}
 	}
 
 	function parseBiometricCSV(stream){
@@ -38,20 +63,18 @@
 				});
 	}
 
-	function writeToDiv(){					/*write first thousand data points to a new <div> above the 'output' <div> */
+	function writeToDiv(string){					/*write first thousand data points to a new <div> above the 'output' <div> */
 
 		var div, dataLine;
-
-		for (var i = 0; i < 1000; i++) {
 			div = document.createElement("div");
-			dataLine = document.createTextNode(biometrics.data[i].date + '   ' + biometrics.data[i].calories +'   '+ biometrics.data[i].gsr);
+			dataLine = document.createTextNode(string);
 			div.appendChild(dataLine);
 			var output = document.getElementById('output');
 			document.body.insertBefore(div, output);
-		}
 
 	}
-	document.biometrics = biometrics;
+
+
 	importBiometrics();
 
 }());
